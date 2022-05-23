d012bd81b2300e232df1c8689a7750cf7fdd0621
Mauricio Figueredo <mauriciofigueredo46@gmail.com>
2022-05-23 16:31:04 -0300

    Experiencia y formacion
    Terminado el maquetado inicial de experiencia y formacion

--- PortfolioWebFullStack/Front-End-Angular/src/app/app.module.ts
+++ PortfolioWebFullStack/Front-End-Angular/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogosComponent } from './components/logos/logos.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { FormacionComponent } from './formacion/formacion.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,9 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     LogosComponent,
     SocialComponent,
     BannerComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperienciaComponent,
+    FormacionComponent
   ],
   imports: [
     BrowserModule
---
+++ PortfolioWebFullStack/Front-End-Angular/src/app/formacion/formacion.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formacion',
+  templateUrl: './formacion.component.html',
+  styles: [
+  ]
+})
+export class FormacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
